8d2e13ceb3dd400371e4dd4c7b1e7d4a26f4c237
ekpono <ekponoambrose@gmail.com>
2022-08-03 06:51:19 +0100

    first commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,11 +11,11 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css', 'packages/form-builder-package') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css', '/vendor/form-builder-package') }}">
 
     <!-- Scripts -->
     @routes
-    <script src="{{ asset('js/app.js', 'packages/form-builder-package') }}" defer></script>
+    <script src="{{ mix('js/app.js', '/vendor/form-builder-package') }}" defer></script>
 </head>
 <body class="font-sans antialiased">
     @inertia
