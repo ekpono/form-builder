b8aa7a85c3ca22535275c882cea89e3b35ade14a
ekpono <ekponoambrose@gmail.com>
2022-09-13 13:48:20 +0100

    remove auth for form runner

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,5 @@ use Shopceed\FormBuilder\Http\Controllers\Pages\FormRunnerController;
 Route::middleware(config('form-builder.default_middleware'))->group(function () {
     Route::get(config('form-builder.path'), [FormBuilderController::class, 'index'])->name('form.builder');
     Route::get(config('form-builder.path').'/{form}', [FormBuilderController::class, 'show'])->name('form.builder.show');
-    Route::get(config('form-builder.path').'/form/{formUuid}/{orderUuid}', [FormRunnerController::class, 'show'])->name('form.show');
 });
+Route::get(config('form-builder.path').'/form/{formUuid}/{orderUuid}', [FormRunnerController::class, 'show'])->name('form.show');
