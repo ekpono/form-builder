6086a2b5128343562cd6068ffbb7c5c57e269788
ekpono <ekponoambrose@gmail.com>
2022-09-14 16:36:09 +0100

    route for guest and api guest created

--- routes/guest.php
+++ routes/guest.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Shopceed\FormBuilder\Http\Controllers\Pages\FormRunnerController;
 
-//Route::get(config('form-builder.path').'/form/{formUuid}/{orderUuid}', [FormRunnerController::class, 'show'])->name('form.show');
+Route::get(config('form-builder.path').'/form/{formUuid}/{orderUuid}', [FormRunnerController::class, 'show'])->name('form.show');
