87907862e0d3c44ac9187cf1b0993c11833751d7
ekpono <ekponoambrose@gmail.com>
2022-09-14 18:45:22 +0100

    reset branch to use branch file

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,13 @@ use Illuminate\Support\Facades\Route;
 use Shopceed\FormBuilder\Http\Controllers\Pages\FormBuilderController;
 use Shopceed\FormBuilder\Http\Controllers\Pages\FormRunnerController;
 
-Route::middleware(config('form-builder.default_middleware'))->group(function () {
-    Route::get(config('form-builder.path'), [FormBuilderController::class, 'index'])->name('form.builder');
-    Route::get(config('form-builder.path').'/{form}', [FormBuilderController::class, 'show'])->name('form.builder.show');
-});
+Route::get(config('form-builder.path'), [FormBuilderController::class, 'index'])
+    ->name('form.builder')
+    ->middleware(config('form-builder.default_middleware'));
+Route::get(config('form-builder.path').'/{form}', [FormBuilderController::class, 'show'])
+    ->name('form.builder.show')
+    ->middleware(config('form-builder.default_middleware'));
+
+Route::get(config('form-builder.path').'/form/{formUuid}/{orderUuid}', [FormRunnerController::class, 'show'])
+    ->name('form.show');
 
