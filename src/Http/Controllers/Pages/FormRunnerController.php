62c2500c92c6b910bce8e8f1a11c8a992c590116
ekpono <ekponoambrose@gmail.com>
2022-09-19 23:15:42 +0100

    merge tags and sytling bug fix

--- src/Http/Controllers/Pages/FormRunnerController.php
+++ src/Http/Controllers/Pages/FormRunnerController.php
@@ -3,15 +3,22 @@
 namespace Shopceed\FormBuilder\Http\Controllers\Pages;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use Shopceed\FormBuilder\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Shopceed\FormBuilder\Models\Form;
 use Shopceed\FormBuilder\Models\FormAnswer;
 use Shopceed\FormBuilder\Models\Order;
+use Shopceed\FormBuilder\Models\Product;
 
 class FormRunnerController extends Controller
 {
+    private $order;
+
+    private $form;
+
     /**
      * @param  Request  $request
      * @param  string  $formUuid
@@ -23,13 +30,14 @@ class FormRunnerController extends Controller
         if ($orderUuid === "{{current_order_id}}") {
             return $this->redirectToPreviewMode($formUuid);
         }
-
         $form = Form::firstWhere('uuid', $formUuid);
         $order = Order::with('store')->firstWhere('uuid', $orderUuid);
 
         if ($form === null || $order === null) {
             throw new ModelNotFoundException();
         }
+
+        $form->data = $this->assignMergeTags($form, $order);
 
         return Inertia::render(
             'FormRunner/Form',
@@ -57,5 +65,79 @@ class FormRunnerController extends Controller
         $form = Form::where('uuid', $formUuid)->first();
 
         return redirect(config('form-builder.path').'/'.$form->id .'?mode=preview');
+    }
+
+    private function assignMergeTags($form, $order)
+    {
+        $this->order = $order;
+        $this->form = $form;
+
+        $m = new \Mustache_Engine(['entity_flags' => ENT_QUOTES]);
+
+        $context = [
+            'customer' => [
+                'first_name' => $this->getVariableValue('customer.first_name'),
+                'last_name' => $this->getVariableValue('customer.last_name'),
+                'full_name' => $this->getVariableValue('customer.full_name'),
+                'email' => $this->getVariableValue('customer.email'),
+            ],
+            'order' => [
+                'total_price' => $this->getVariableValue('total_price'),
+                'number' => $this->getVariableValue('number'),
+                'line_items_count' => $this->getVariableValue('line_items_count'),
+            ],
+            'products' => [
+                'name' => $this->productName(),
+            ],
+        ];
+
+        $data = $m->render(json_encode($form->data), $context);
+
+        return json_decode($data, true);
+    }
+
+    private function getVariableValue($variableName)
+    {
+        // Retrieve a single product from line_items
+        if (Str::startsWith($variableName, 'product.')) {
+            $key = 'line_items.0.'.str_replace('product.', '', $variableName);
+
+            return Arr::get($this->order->data, $key);
+        }
+
+        return Arr::get(json_decode($this->order->data, true), $variableName);
+    }
+
+    private function productName()
+    {
+        $productNames = Arr::pluck($this->getProducts(), 'name');
+
+        if (empty($productNames)) {
+            return 'No product';
+        }
+
+        $totalProductCount = count($this->getProducts());
+
+        if ($totalProductCount === 1) {
+            return $productNames[0];
+        }
+
+        if ($totalProductCount === 2) {
+            return $productNames[0].' and '.$productNames[1];
+        }
+
+        return $productNames[0].'and'.$totalProductCount.'others';
+    }
+
+    private function getProducts(): array
+    {
+        $lineItems = $this->getVariableValue('line_items');
+        $externalIds = collect($lineItems)->pluck('external_product_id')->all();
+
+        return Product::select('name', 'data->image as image')
+            ->where('store_id', $this->order->store_id)
+            ->whereIn('external_id', $externalIds)
+            ->get()
+            ->toArray();
     }
 }
