2ee965ab62c89d902b88d521da5aa879936dca54
ekpono <ekponoambrose@gmail.com>
2022-09-29 13:58:07 +0100

    image added to form runner

--- src/Models/Order.php
+++ src/Models/Order.php
@@ -27,4 +27,30 @@ class Order extends Model
         return $this->hasMany(OrderLineItem::class, 'external_id', 'external_id');
     }
 
+    public function getDataAttribute()
+    {
+        $items = [];
+        $data = json_decode($this->attributes['data'], true);
+        foreach ($data['line_items'] as $item) {
+            $setImage = false;
+            if (class_exists('App\Models\Product')) {
+                $product = \App\Models\Product::query()->where('external_id', $item['external_product_id'])->first();
+                if ($product) {
+                    $item['image'] = $product->main_image_url;
+                    $setImage = true;
+                }
+            }
+
+            if (! $setImage) {
+                $item['image'] = 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png';
+            }
+
+            array_push($items, $item);
+        }
+
+        $data['line_items'] = $items;
+
+        return json_encode($data);
+    }
+
 }
